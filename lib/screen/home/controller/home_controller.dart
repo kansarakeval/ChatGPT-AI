d364e4295a09cc2469f9ec2ca8655ef57aaa329d
os <kkeval304@gmail.com>
2024-05-03 20:22:43 +0530

    new

--- lib/screen/home/controller/home_controller.dart
+++ lib/screen/home/controller/home_controller.dart
@@ -1,5 +1,7 @@
+import 'package:advance_exam_app/screen/history/model/history_model.dart';
 import 'package:advance_exam_app/screen/home/model/home_model.dart';
 import 'package:advance_exam_app/util/api_helper.dart';
+import 'package:advance_exam_app/util/db_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
@@ -16,7 +18,9 @@ class HomeController extends GetxController {
     HomeModel? w1 = await apiHelper.homeAPICall(edit);
     homeModel?.value = w1;
     if (homeModel!.value != null) {
-      chatHistory.add(homeModel!.value!.candidateList![0].content!.partsList![0].text!);
+      var model = HistoryModel(answer: w1!.candidateList![0].content!.partsList![0].text!);
+      DbHelper.helper.insertAiData(model);
+      chatHistory.add(w1!.candidateList![0].content!.partsList![0].text!);
     }
     isLoding.value = false;
   }
--- lib/screen/home/view/home_screen.dart
+++ lib/screen/home/view/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:advance_exam_app/screen/home/controller/home_controller.dart';
 import 'package:advance_exam_app/util/db_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
@@ -16,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   HomeController controller = Get.put(HomeController());
   TextEditingController txtedit = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -40,77 +42,98 @@ class _HomeScreenState extends State<HomeScreen> {
             )
           ],
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Obx(
-                () => ListView.builder(
-                  reverse: true,
-                  itemCount: controller.chatHistory.length,
-                  itemBuilder: (context, index) {
-                    final message = controller.chatHistory.length - index - 1;
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(6),
-                          margin: const EdgeInsets.all(6),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: ([...Colors.primaries]..shuffle())
-                                .first
-                                .shade100,
-                          ),
-                          child: Text(
-                            controller.chatHistory[message],
-                            style: const TextStyle(fontSize: 17),
-                          ),
+        body: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Expanded(
+                child: Obx(
+                      () => ListView.builder(
+                    reverse: true,
+                    itemCount: controller.chatHistory.length,
+                    itemBuilder: (context, index) {
+                      final message = controller.chatHistory.length - index - 1;
+                      bool isQuestion = index % 2 == 0;
+                      return Container(
+                        alignment: isQuestion
+                            ? Alignment.centerLeft
+                            : Alignment.centerRight,
+                        padding: const EdgeInsets.all(6),
+                        margin: const EdgeInsets.all(6),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: isQuestion
+                              ? ([...Colors.primaries]..shuffle())
+                              .first
+                              .shade100
+                              : ([...Colors.primaries]..shuffle())
+                              .first
+                              .shade100,
                         ),
-                      ],
-                    );
-                  },
+                        child: Text(
+                          controller.chatHistory[message],
+                          style: const TextStyle(fontSize: 17),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: TextFormField(
-                      controller: txtedit,
-                      decoration: InputDecoration(
-                        labelText: 'Search',
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        fillColor: Colors.white,
-                        suffixIcon: ValueListenableBuilder(
-                          valueListenable: controller.isLoding,
-                          builder: (context, value, child) {
-                            if (value) {
-                              return const CircularProgressIndicator();
-                            }
-                            return IconButton(
-                              onPressed: () async {
-                                String edit = txtedit.text;
-                                controller.chatHistory.add(edit);
-                                HistoryModel model =
-                                    HistoryModel(answer: txtedit.text);
-                                DbHelper.helper.insertAiData(model);
-                                await controller.getHomeData(edit);
-                                txtedit.clear();
-                              },
-                              icon: const Icon(Icons.send),
-                            );
-                          },
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: TextFormField(
+                        controller: txtedit,
+                        decoration: InputDecoration(
+                          labelText: 'Search',
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.white,
+                          suffixIcon: ValueListenableBuilder(
+                            valueListenable: controller.isLoding,
+                            builder: (context, value, child) {
+                              if (value) {
+                                return const SizedBox(
+                                  width: 20,
+                                  height: 20,
+                                  child: SpinKitThreeBounce(
+                                    color: Colors.blue,
+                                    size: 20,
+                                  ),
+                                );
+                              }
+                              return IconButton(
+                                onPressed: () async {
+                                  if (formKey.currentState!.validate()) {
+                                    String edit = txtedit.text;
+                                    controller.chatHistory.add(edit);
+                                    HistoryModel model = HistoryModel(answer: txtedit.text);
+                                    DbHelper.helper.insertAiData(model);
+                                    await controller.getHomeData(edit);
+                                    txtedit.clear();
+                                    FocusScope.of(context).unfocus();
+                                  }
+                                },
+                                icon: const Icon(Icons.send),
+                              );
+                            },
+                          ),
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
